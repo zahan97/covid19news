6e17046ec8f4a82d61a775f6f444e6c9cf0f3a19
zahan97 <zahan.safe@gmail.com>
2020-08-08 11:53:16 -0700

    Updates

---
+++ precache-manifest.89149734c30c4790221118e4bd80772e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ee4bcffc7c0038f9c32ad84abebcbd",
+    "url": "/covid19news/index.html"
+  },
+  {
+    "revision": "abda97bd3b8522c35915",
+    "url": "/covid19news/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "defb22a806821f3894eb",
+    "url": "/covid19news/static/css/main.fb34b169.chunk.css"
+  },
+  {
+    "revision": "abda97bd3b8522c35915",
+    "url": "/covid19news/static/js/2.bd949dc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19news/static/js/2.bd949dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defb22a806821f3894eb",
+    "url": "/covid19news/static/js/main.e8d06cfb.chunk.js"
+  },
+  {
+    "revision": "e57a4b91061635984001",
+    "url": "/covid19news/static/js/runtime-main.07f4a2ec.js"
+  }
+]);
